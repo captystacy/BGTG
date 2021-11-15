e301dd5e0a5ac7492456358b3fc8aa39c06708c9
kss <kss@37_414_3>
2021-11-15 15:03:13 +0300

    add test to costructor of CalendarPlanCreator

---
+++ POSCoreTests/CalendarPlanCreatorTests/ConstructorShould.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using NUnit.Framework;
+using POSCore.CalendarPlanLogic;
+using POSCore.EstimateLogic.Interfaces;
+
+namespace POSCoreTests.CalendarPlanCreatorTests
+{
+    public class ConstructorShould
+    {
+        private Mock<IEstimate> _estimateVatFree;
+        private Mock<IEstimate> _estimateVat;
+        private Mock<IEstimateConnector> _estimateConnectorMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _estimateVatFree = new Mock<IEstimate>();
+            _estimateVat = new Mock<IEstimate>();
+            _estimateConnectorMock = new Mock<IEstimateConnector>();
+        }
+
+        [Test]
+        public void ConnectEstimates()
+        {
+            new CalendarPlanCreator(_estimateVatFree.Object, _estimateVat.Object, _estimateConnectorMock.Object);
+            _estimateConnectorMock.Verify(x => x.Connect(_estimateVatFree.Object, _estimateVat.Object), Times.Once);
+        }
+    }
+}
