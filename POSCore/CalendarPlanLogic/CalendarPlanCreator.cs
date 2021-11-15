3eb552618e760c6c3ce43eede41241e8e7e237c9
kss <kss@37_414_3>
2021-11-15 13:47:05 +0300

    add NotNull test to CalendarPlanCreator

---
+++ POSCore/CalendarPlanLogic/CalendarPlanCreator.cs
@@ -0,0 +1,27 @@
+﻿using POSCore.CalendarPlanLogic.Interfaces;
+using POSCore.EstimateLogic.Interfaces;
+using System;
+
+namespace POSCore.CalendarPlanLogic
+{
+    public class CalendarPlanCreator : ICalendarPlanCreator
+    {
+        private readonly IEstimateConnector _estimateConnector;
+        private readonly IEstimate _estimate;
+
+        public CalendarPlanCreator(IEstimate estimateVatFree)
+        {
+            _estimate = estimateVatFree;
+        }
+
+        public CalendarPlanCreator(IEstimate estimateVatFree, IEstimate estimateVat, IEstimateConnector estimateConnector)
+        {
+            _estimate = estimateConnector.Connect(estimateVatFree, estimateVat);
+        }
+
+        public ICalendarPlan CreateCalendarPlan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ POSCoreTests/CalendarPlanCreatorTests/CreateCalendarPlanShould.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using POSCore.CalendarPlanLogic;
+using POSCore.CalendarPlanLogic.Interfaces;
+using POSCore.EstimateLogic.Interfaces;
+
+namespace POSCoreTests.CalendarPlanCreatorTests
+{
+    public class CreateCalendarPlanShould
+    {
+        private ICalendarPlanCreator _calendarPlanCreator;
+        private Mock<IEstimate> _estimateVatFree;
+        private Mock<IEstimate> _estimateVat;
+        private Mock<IEstimateConnector> _estimateConnectorMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _estimateVatFree = new Mock<IEstimate>();
+            _estimateVat = new Mock<IEstimate>();
+            _estimateConnectorMock = new Mock<IEstimateConnector>();
+            _calendarPlanCreator = new CalendarPlanCreator(_estimateVatFree.Object, _estimateVat.Object, _estimateConnectorMock.Object);
+        }
+
+        [Test]
+        public void NotReturnNull()
+        {
+            var calendarPlan = _calendarPlanCreator.CreateCalendarPlan();
+
+            Assert.NotNull(calendarPlan);
+        }
+    }
+}
