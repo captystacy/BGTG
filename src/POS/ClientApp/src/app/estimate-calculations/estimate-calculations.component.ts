0554a0d2880085df13a1c826f868b48518dd8497
captystacy <captystacy@gmail.com>
2022-03-03 20:42:53 +0300

    add demo estimate-calculations component

---
+++ src/POS/ClientApp/src/app/estimate-calculations/estimate-calculations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estimate-calculations',
+  templateUrl: './estimate-calculations.component.html',
+  styleUrls: ['./estimate-calculations.component.css']
+})
+export class EstimateCalculationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
